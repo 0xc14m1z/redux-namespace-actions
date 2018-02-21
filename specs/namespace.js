25286b51149d4c560ed7091dfe85c68d5c530f5c
ciamiz <luca@ramundo.info>
2018-02-21 16:20:28 +0100

    Refine specs and namespace method

--- specs/namespace.js
+++ specs/namespace.js
@@ -12,51 +12,77 @@ describe("namespace", () => {
     expect(test).to.be.equal(expected)
   })
 
-  it("should leave blank the namespace if it is null", () => {
-    const namespaceName = null
-    const actionName = "ACTION"
-    const test = namespace(namespaceName)(actionName)
-    const expected = "/ACTION"
-    expect(test).to.be.equal(expected)
-  })
-
-  it("should leave blank the namespace if it is undefined", () => {
-    const namespaceName = undefined
-    const actionName = "ACTION"
-    const test = namespace(namespaceName)(actionName)
-    const expected = "/ACTION"
-    expect(test).to.be.equal(expected)
-  })
-
-  it("should leave blank the action if it is null", () => {
-    const namespaceName = "NAMESPACE"
-    const actionName = null
-    const test = namespace(namespaceName)(actionName)
-    const expected = "NAMESPACE/"
-    expect(test).to.be.equal(expected)
-  })
-
-  it("should leave blank the action if it is undefined", () => {
-    const namespaceName = "NAMESPACE"
-    const actionName = undefined
-    const test = namespace(namespaceName)(actionName)
-    const expected = "NAMESPACE/"
-    expect(test).to.be.equal(expected)
-  })
-
   describe("should throw an error if the namespace isn't a string", () => {
+
+    it("when it's null", () => {
+      const namespaceName = null
+      const actionName = "ACTION"
+      const test = () => namespace(namespaceName)(actionName)
+      expect(test).to.throw(TypeError)
+    })
+
+    it("when it's undefined", () => {
+      const namespaceName = undefined
+      const actionName = "ACTION"
+      const test = () => namespace(namespaceName)(actionName)
+      expect(test).to.throw(TypeError)
+    })
+
+    it("when it isn't passed as argument", () => {
+      const actionName = "ACTION"
+      const test = () => namespace()(actionName)
+      expect(test).to.throw(TypeError)
+    })
 
     it("when it's an array", () => {
       const namespaceName = []
       const actionName = "ACTION"
-      const test = namespace(namespaceName)(actionName)
+      const test = () => namespace(namespaceName)(actionName)
       expect(test).to.throw(TypeError)
     })
 
     it("when it's an object", () => {
       const namespaceName = {}
       const actionName = "ACTION"
-      const test = namespace(namespaceName)(actionName)
+      const test = () => namespace(namespaceName)(actionName)
+      expect(test).to.throw(TypeError)
+    })
+
+  })
+
+  describe("should throw an error if the action isn't a string", () => {
+
+    it("when it's null", () => {
+      const namespaceName = "NAMESPACE"
+      const actionName = null
+      const test = () => namespace(namespaceName)(actionName)
+      expect(test).to.throw(TypeError)
+    })
+
+    it("when it's undefined", () => {
+      const namespaceName = "NAMESPACE"
+      const actionName = undefined
+      const test = () => namespace(namespaceName)(actionName)
+      expect(test).to.throw(TypeError)
+    })
+
+    it("when it isn't passed as argument", () => {
+      const namespaceName = "NAMESPACE"
+      const test = () => namespace(namespaceName)()
+      expect(test).to.throw(TypeError)
+    })
+
+    it("when it's an array", () => {
+      const namespaceName = "NAMESPACE"
+      const actionName = []
+      const test = () => namespace(namespaceName)(actionName)
+      expect(test).to.throw(TypeError)
+    })
+
+    it("when it's an object", () => {
+      const namespaceName = "NAMESPACE"
+      const actionName = {}
+      const test = () => namespace(namespaceName)(actionName)
       expect(test).to.throw(TypeError)
     })
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-const namespace = namespaceName => actionName =>
-  `${namespaceName}/${actionName}`
+const namespace = (namespaceName) => (actionName) =>
+  validate(namespaceName, actionName) && join(namespaceName, actionName)
+
+const join = (...args) =>
+  args.join("/")
+
+const validate = (namespaceName, actionName) => {
+  if ( !isString(namespaceName) ) {
+    throw new TypeError("The namespace name must be a string.")
+  } else if ( !isString(actionName) ) {
+    throw new TypeError("The action name must be a string.")
+  }
+  return true
+}
+
+const isString = s =>
+  typeof s === "string"
 
 export default namespace
 module.exports = namespace
